6ead8896ca439ff104db42f6d647ba67bff46242
Bastien <bmoulia@gmail.com>
2020-04-08 20:17:15 +0200

    fix shell css

--- src/app/app-shell/app-shell.component.ts
+++ src/app/app-shell/app-shell.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-app-shell',
   templateUrl: './app-shell.component.html',
-  styleUrls: ['./app-shell.component.scss']
+  styleUrls: ['./app-shell.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppShellComponent implements OnInit {
 
