e02d4c6d93dc8f4c384f7c7705d5d1e24ecbd945
Bastien <bmoulia@gmail.com>
2020-04-09 00:16:58 +0200

    notification

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,6 +10,11 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if (!('Notification' in window)) {
+      console.warn('This browser does not support desktop notification');
+    } else if (Notification.permission !== 'granted') {
+      Notification.requestPermission();
+    }
   }
 
 }
